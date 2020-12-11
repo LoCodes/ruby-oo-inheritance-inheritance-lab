85d7f84444b89ee3fd85b60b3a8ae4ea59c28f0f
Loren Cipriano <loren25cipriano@gmail.com>
2020-12-10 16:42:34 -0800

    commit tree 4df68e30e302169585e59530d9cf90a8b7347b28

---
+++ lib/user.rb
@@ -0,0 +1,14 @@
+
+# User
+#   has a first name
+#   has a last name
+
+
+class User
+    attr_accessor :first_name, :last_name
+
+    def initialize #(first_name, last_name)
+        @first_name = first_name
+        @last_name = last_name
+    end 
+end 
